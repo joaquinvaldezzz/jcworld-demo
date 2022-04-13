aaf6de46139a05b8f8efc8a21fcecf345a5b5d03
John Joaquin Valdez <javaldez1642qc@student.fatima.edu.ph>
2022-04-13 14:59:06 +0800

    Install Tailwind CSS

--- postcss.config.js
+++ postcss.config.js
@@ -1,22 +1,16 @@
-const path = require('path');
-const postcssSorting = require('./util/postcssSorting');
-
 module.exports = {
   plugins: {
     autoprefixer: {},
     '@fullhuman/postcss-purgecss': process.env.NODE_ENV === 'production' && {
-      content: [path.join(__dirname, 'index.html'), path.join(__dirname, 'src/**/*.js')],
+      content: ['./index.html', './src/**/*.js'],
       defaultExtractor: (content) => {
-        const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]+/g) || [];
-        const innerMatches = content.match(/[^<>"'`\s.()]*[^<>"'`\s.():]+/g) || [];
-        return broadMatches.concat(innerMatches);
+        const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]+/g) || []
+        const innerMatches = content.match(/[^<>"'`\s.()]*[^<>"'`\s.():]+/g) || []
+        return broadMatches.concat(innerMatches)
       },
     },
+    'postcss-import': {},
     'postcss-sort-media-queries': {},
-    'postcss-sorting': {
-      order: [...postcssSorting.order],
-      'properties-order': [...postcssSorting.propertiesOrder],
-      'unspecified-properties-position': 'bottom',
-    },
+    tailwindcss: {},
   },
-};
+}
