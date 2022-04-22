bb74f2f909297180c8a06c2d9bfbd5b7157f66c9
John Joaquin Valdez <javaldez1642qc@student.fatima.edu.ph>
2022-04-22 13:30:29 +0800

    Create Blog Posts section

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,9 @@
 <template>
-  <main>
-    <section class="bg-azure py-8 sm:py-12 md:py-16">
+  <main class="bg-azure">
+    <section class="py-8 sm:py-12 md:py-16">
       <div class="container">
         <div class="text-center text-sm uppercase tracking-wide text-vulcan">Get to know</div>
-        <h2 class="text-center text-xl uppercase md:text-3xl lg:text-4xl">About Us</h2>
+        <h2 class="text-center text-xl uppercase md:text-3xl lg:mt-0.5 lg:text-4xl">About Us</h2>
         <div class="mt-8" role="grid">
           <div class="grid gap-8 lg:grid-cols-2" role="row">
             <div role="gridcell">
@@ -17,6 +17,65 @@
 
             <div role="gridcell">
               <img class="rounded-2xl md:mx-auto" src="/images/jc-world.jpg" alt="JC World" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="relative overflow-hidden py-8 sm:py-12 md:py-16">
+      <img class="hidden xl:absolute xl:top-0 xl:left-0 xl:block xl:select-none" src="/images/tree-edge-left.png" alt="Left Tree" />
+      <img class="absolute top-0 right-0 select-none" src="/images/tree-edge-right.png" alt="Right Tree" />
+      <div class="container">
+        <div class="relative">
+          <div class="text-center text-sm uppercase tracking-wide text-vulcan">Our Latest</div>
+          <h2 class="text-center text-xl uppercase md:text-3xl lg:mt-0.5 lg:text-4xl">Blog Posts</h2>
+          <div class="mt-8 sm:mx-auto sm:max-w-sm md:mt-12 lg:mt-16 lg:max-w-none" role="grid">
+            <div class="grid gap-8 lg:mx-auto lg:max-w-5xl lg:grid-cols-3" role="row">
+              <div role="gridcell">
+                <div class="overflow-hidden rounded-2xl bg-alice-blue">
+                  <div>
+                    <img class="aspect-4/3 object-cover" src="https://img.freepik.com/free-photo/children-play-pop-it-street_73944-17638.jpg?w=1080" alt="Children play pop it on the street" />
+                  </div>
+                  <div class="px-4 py-5">
+                    <h3 class="text-2xl font-semibold text-vulcan">Lorem ipsum dolor sit.</h3>
+                    <p class="mt-5 text-grey">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facere consequuntur sunt quidem quisquam voluptates at porro.</p>
+                    <div class="mt-5">
+                      <a class="inline-block rounded-lg bg-burnt-sienna px-6 py-2 text-sm uppercase tracking-wider text-white" href="#">Learn more</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div role="gridcell">
+                <div class="overflow-hidden rounded-2xl bg-alice-blue">
+                  <div>
+                    <img class="aspect-4/3 object-cover" src="https://images.unsplash.com/photo-1621354598022-16599af1b8b2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1080&q=80" alt="Girl in blue shirt lying on yellow inflatable pool" />
+                  </div>
+                  <div class="px-4 py-5">
+                    <h3 class="text-2xl font-semibold text-vulcan">Lorem ipsum dolor sit.</h3>
+                    <p class="mt-5 text-grey">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facere consequuntur sunt quidem quisquam voluptates at porro.</p>
+                    <div class="mt-5">
+                      <a class="inline-block rounded-lg bg-burnt-sienna px-6 py-2 text-sm uppercase tracking-wider text-white" href="#">Learn more</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div role="gridcell">
+                <div class="overflow-hidden rounded-2xl bg-alice-blue">
+                  <div>
+                    <img class="aspect-4/3 object-cover" src="https://images.unsplash.com/photo-1502086223501-7ea6ecd79368?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1080&q=80" alt="Four boy playing ball on green grass" />
+                  </div>
+                  <div class="px-4 py-5">
+                    <h3 class="text-2xl font-semibold text-vulcan">Lorem ipsum dolor sit.</h3>
+                    <p class="mt-5 text-grey">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facere consequuntur sunt quidem quisquam voluptates at porro.</p>
+                    <div class="mt-5">
+                      <a class="inline-block rounded-lg bg-burnt-sienna px-6 py-2 text-sm uppercase tracking-wider text-white" href="#">Learn more</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
