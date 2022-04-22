fafc6b5b74d89ff3826e33a6d443620ebc836c17
John Joaquin Valdez <javaldez1642qc@student.fatima.edu.ph>
2022-04-22 11:08:11 +0800

    Extend colors and box shadows

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,14 +6,22 @@ module.exports = {
     container: {
       center: true,
       padding: {
-        DEFAULT: '1rem',
+        DEFAULT: '2rem',
       },
     },
     extend: {
       colors: {
         'jungle-green': '#2db97a',
         sunglow: '#ffce31',
+        vulcan: '#35393e',
+        grey: '#7a7979',
+        'burnt-sienna': '#ed5f38',
+        azure: '#f8ffff',
         'maya-blue': '#4ab5f8',
+      },
+      boxShadow: {
+        sm: '0 4px 4px 0 rgba(0 0 0 / 0.25)',
+        xl: '0 0 14px 0px rgba(0 0 0 / 0.25)',
       },
     },
   },
