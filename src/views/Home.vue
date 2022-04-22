f3ba0cc73fc764a118e757f1c87eeffe46c49f6a
John Joaquin Valdez <javaldez1642qc@student.fatima.edu.ph>
2022-04-22 11:07:48 +0800

    Create About Us section

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,26 @@
 <template>
-  <main></main>
+  <main>
+    <section class="bg-azure py-8 sm:py-12 md:py-16">
+      <div class="container">
+        <div class="text-center text-sm uppercase tracking-wide text-vulcan">Get to know</div>
+        <h2 class="text-center text-xl uppercase md:text-3xl lg:text-4xl">About Us</h2>
+        <div class="mt-8" role="grid">
+          <div class="grid gap-8 lg:grid-cols-2" role="row">
+            <div role="gridcell">
+              <h3 class="text-xl font-semibold text-vulcan md:text-3xl lg:text-4xl">Our Mission</h3>
+              <p class="mt-5 text-grey">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat mollitia esse voluptas laborum minima ad corrupti. Repellendus mollitia in repellat veritatis eligendi perferendis placeat suscipit nulla consequatur sed nisi, iure assumenda quaerat.</p>
+              <p class="mt-5 text-grey">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus nam porro laborum earum labore iure in? Incidunt quos ipsum minima eius possimus, aperiam quam eligendi laboriosam neque repellendus saepe, asperiores, quas perspiciatis? Quod obcaecati enim modi sit quidem repellat quae doloremque reiciendis beatae, molestias sequi eligendi voluptatum maxime. Rem facere magni at dignissimos dolores non. Obcaecati esse quas architecto maiores error quia, quam dolores!</p>
+              <div class="mt-5">
+                <a class="inline-block rounded-full bg-burnt-sienna px-6 py-2 text-sm uppercase tracking-wider text-white" href="#">Contact Us</a>
+              </div>
+            </div>
+
+            <div role="gridcell">
+              <img class="rounded-2xl md:mx-auto" src="/images/jc-world.jpg" alt="JC World" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </main>
 </template>
