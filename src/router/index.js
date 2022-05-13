f43d650411e3507d2eefe1441b87722848e06539
John Joaquin Valdez <javaldez1642qc@student.fatima.edu.ph>
2022-05-13 22:50:24 +0800

    Create FAQ page

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import FAQ from '../views/FAQ.vue'
 
 const routes = [
   {
     path: '/',
     name: 'Home',
     component: Home,
+  },
+  {
+    path: '/faq',
+    name: 'FAQ',
+    component: FAQ,
   },
 ]
 
---
+++ src/views/FAQ.vue
@@ -0,0 +1,95 @@
+<template>
+  <main>
+    <section class="relative bg-charlotte py-8 sm:py-32 md:pt-48 lg:pt-56 xl:pt-80 xl:pb-56">
+      <img class="hidden sm:absolute sm:inset-x-0 sm:top-0 sm:block sm:select-none lg:mx-auto lg:max-w-4xl xl:max-w-6xl" src="/images/clouds-and-stars.png" alt="Clouds and stars" />
+      <img class="absolute inset-x-0 bottom-0 select-none" src="/svg/clouds.svg" alt="Clouds" />
+      <div class="container relative">
+        <div class="text-center text-sm uppercase tracking-wide text-vulcan">FAQs</div>
+        <h1 class="mt-0.5 text-center text-2xl font-bold uppercase tracking-tight sm:text-3xl md:text-4xl lg:text-5xl">Frequently Asked Questions</h1>
+        <p class="mt-4 text-center lg:mx-auto lg:max-w-xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor.</p>
+      </div>
+    </section>
+
+    <section class="py-8 sm:py-12 md:py-16">
+      <div class="container">
+        <div class="space-y-4 divide-y lg:mx-auto lg:max-w-xl" aria-label="Accordion group">
+          <div aria-label="Accordion item">
+            <button class="flex w-full items-center justify-between text-left text-xl font-semibold" aria-label="Accordion button">
+              <span>Lorem ipsum dolor sit.</span>
+              <svg class="h-6 w-6 fill-current text-maya-blue" aria-label="Expand icon" viewBox="0 0 24 24">
+                <path d="M16.313 11.25H12.75V7.687a.188.188 0 0 0-.188-.187h-1.124a.188.188 0 0 0-.188.188v3.562H7.687a.188.188 0 0 0-.187.188v1.124c0 .104.084.188.188.188h3.562v3.563c0 .103.084.187.188.187h1.124a.188.188 0 0 0 .188-.188V12.75h3.563a.188.188 0 0 0 .187-.188v-1.124a.188.188 0 0 0-.188-.188Z" />
+                <path d="M12 1.5C6.202 1.5 1.5 6.202 1.5 12S6.202 22.5 12 22.5 22.5 17.798 22.5 12 17.798 1.5 12 1.5Zm0 19.219A8.72 8.72 0 0 1 3.281 12 8.72 8.72 0 0 1 12 3.281 8.72 8.72 0 0 1 20.719 12 8.72 8.72 0 0 1 12 20.719Z" />
+              </svg>
+              <svg class="hidden h-6 w-6 fill-current text-maya-blue" aria-label="Collapse icon" viewBox="0 0 24 24">
+                <path d="M7 11h10v2H7v-2Z" />
+                <path d="M12 2C6.486 2 2 6.486 2 12s4.486 10 10 10 10-4.486 10-10S17.514 2 12 2Zm0 18c-4.411 0-8-3.589-8-8s3.589-8 8-8 8 3.589 8 8-3.589 8-8 8Z" />
+              </svg>
+            </button>
+
+            <div class="mt-2 hidden text-grey" aria-label="Accordion content">
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum aperiam ipsam repellendus recusandae commodi deserunt cum.</p>
+            </div>
+          </div>
+
+          <div class="pt-4" aria-label="Accordion item">
+            <button class="flex w-full items-center justify-between text-left text-xl font-semibold" aria-label="Accordion button">
+              <span>Lorem ipsum dolor sit.</span>
+              <svg class="h-6 w-6 fill-current text-maya-blue" aria-label="Expand icon" viewBox="0 0 24 24">
+                <path d="M16.313 11.25H12.75V7.687a.188.188 0 0 0-.188-.187h-1.124a.188.188 0 0 0-.188.188v3.562H7.687a.188.188 0 0 0-.187.188v1.124c0 .104.084.188.188.188h3.562v3.563c0 .103.084.187.188.187h1.124a.188.188 0 0 0 .188-.188V12.75h3.563a.188.188 0 0 0 .187-.188v-1.124a.188.188 0 0 0-.188-.188Z" />
+                <path d="M12 1.5C6.202 1.5 1.5 6.202 1.5 12S6.202 22.5 12 22.5 22.5 17.798 22.5 12 17.798 1.5 12 1.5Zm0 19.219A8.72 8.72 0 0 1 3.281 12 8.72 8.72 0 0 1 12 3.281 8.72 8.72 0 0 1 20.719 12 8.72 8.72 0 0 1 12 20.719Z" />
+              </svg>
+              <svg class="hidden h-6 w-6 fill-current text-maya-blue" aria-label="Collapse icon" viewBox="0 0 24 24">
+                <path d="M7 11h10v2H7v-2Z" />
+                <path d="M12 2C6.486 2 2 6.486 2 12s4.486 10 10 10 10-4.486 10-10S17.514 2 12 2Zm0 18c-4.411 0-8-3.589-8-8s3.589-8 8-8 8 3.589 8 8-3.589 8-8 8Z" />
+              </svg>
+            </button>
+
+            <div class="mt-2 hidden text-grey" aria-label="Accordion content">
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum aperiam ipsam repellendus recusandae commodi deserunt cum.</p>
+            </div>
+          </div>
+
+          <div class="pt-4" aria-label="Accordion item">
+            <button class="flex w-full items-center justify-between text-left text-xl font-semibold" aria-label="Accordion button">
+              <span>Lorem ipsum dolor sit.</span>
+              <svg class="h-6 w-6 fill-current text-maya-blue" aria-label="Expand icon" viewBox="0 0 24 24">
+                <path d="M16.313 11.25H12.75V7.687a.188.188 0 0 0-.188-.187h-1.124a.188.188 0 0 0-.188.188v3.562H7.687a.188.188 0 0 0-.187.188v1.124c0 .104.084.188.188.188h3.562v3.563c0 .103.084.187.188.187h1.124a.188.188 0 0 0 .188-.188V12.75h3.563a.188.188 0 0 0 .187-.188v-1.124a.188.188 0 0 0-.188-.188Z" />
+                <path d="M12 1.5C6.202 1.5 1.5 6.202 1.5 12S6.202 22.5 12 22.5 22.5 17.798 22.5 12 17.798 1.5 12 1.5Zm0 19.219A8.72 8.72 0 0 1 3.281 12 8.72 8.72 0 0 1 12 3.281 8.72 8.72 0 0 1 20.719 12 8.72 8.72 0 0 1 12 20.719Z" />
+              </svg>
+              <svg class="hidden h-6 w-6 fill-current text-maya-blue" aria-label="Collapse icon" viewBox="0 0 24 24">
+                <path d="M7 11h10v2H7v-2Z" />
+                <path d="M12 2C6.486 2 2 6.486 2 12s4.486 10 10 10 10-4.486 10-10S17.514 2 12 2Zm0 18c-4.411 0-8-3.589-8-8s3.589-8 8-8 8 3.589 8 8-3.589 8-8 8Z" />
+              </svg>
+            </button>
+
+            <div class="mt-2 hidden text-grey" aria-label="Accordion content">
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum aperiam ipsam repellendus recusandae commodi deserunt cum.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </main>
+</template>
+
+<script>
+const accordion = () => {
+  document.querySelectorAll('[aria-label="Accordion button"]').forEach((accordionButton) => {
+    accordionButton.addEventListener('click', () => {
+      const accordionContent = accordionButton.nextElementSibling
+
+      accordionContent.classList.toggle('hidden')
+    })
+  })
+}
+
+export default {
+  methods: {
+    accordion,
+  },
+  mounted: () => {
+    document.title = 'Frequently Asked Questions'
+    accordion.call(this)
+  },
+}
+</script>
